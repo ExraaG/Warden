7b1a99f0ae24a8dd8559bb03a063d735db23860d
AlexH <alexh@warden.local>
2026-08-20 14:57:40 +0200

    chore(main): sync release build

--- server/src/version.ts
+++ server/src/version.ts
@@ -1,6 +1,6 @@
-// Auto-generated hardcoded version: v4-c01bc10fa
-export const WARDEN_VERSION = 'v4-c01bc10fa';
+// Auto-generated hardcoded version: v4-be37d5891
+export const WARDEN_VERSION = 'v4-be37d5891';
 export const WARDEN_VERSION_NUMBER = 4;
-export const WARDEN_GIT_HASH = 'c01bc10fa';
+export const WARDEN_GIT_HASH = 'be37d5891';
 export const WARDEN_RELEASE_TITLE = "v4 (c5a8da5d4)";
-export const WARDEN_BUILD_TIME = '2026-08-20T12:50:43.364Z';
+export const WARDEN_BUILD_TIME = '2026-08-20T12:57:16.052Z';
